13ce0b6f03013d2d85785a5e767ce1f14f5bfc81
Jascha030 <contact@jaschavanaalst.nl>
2022-05-16 10:42:27 +0200

    ✨ Add shell function

--- interface-style-observer.swift
+++ interface-style-observer.swift
@@ -2,3 +2,15 @@
 
 import Cocoa;
 
+@discardableResult
+func shell(_ args: String...) -> Int32 {
+    let task = Process()
+    
+    task.launchPath = "/usr/bin/env"
+    task.arguments = args
+    
+    task.launch()
+    task.waitUntilExit()
+    
+    return task.terminationStatus
+}
